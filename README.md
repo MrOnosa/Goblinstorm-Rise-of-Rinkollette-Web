c34f5c44f3e28d5a5b3bda692593b96f1d2e066f
MrOnosa <supersnapple@gmail.com>
2024-07-09 05:49:37 -0500

    Removed "Quit" button from victory screen. Fixed inventory item not showing up issue. Tried to reduce lag by loading sprites on the title screen out of frame.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# learn-you-a-game-jam-2024
+# Goblinstorm: Rise of Rinkollette (Web port)
+
+The source code for https://mronosa.itch.io/goblinstorm-rise-of-rinkollette-web
+
+Ported https://github.com/MrOnosa/learn-you-a-game-jam-2024 from C# to GDScript
--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -1,13 +1,13 @@
 extends TextureRect
 
-var _animatedSprite2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 
 func _ready() -> void:
-	_animatedSprite2D = get_node("AnimatedSprite2D")
-	_animatedSprite2D.frame = int(Constants.ItemType.None)
+	animated_sprite_2d.frame = int(Constants.ItemType.None)
 
 func _process(delta) -> void:
 	pass
 
 func _on_witch_item_changed(itemType):
-	_animatedSprite2D.frame = int(itemType)
+	animated_sprite_2d.frame = int(itemType)
